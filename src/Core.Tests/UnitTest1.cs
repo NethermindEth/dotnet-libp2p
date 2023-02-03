e5993dca86e448c52e2df7c15659cfc657e8e5d2
Ruben Buniatyan <rubo@users.noreply.github.com>
2023-02-03 17:21:51 +0100

    Add Core project

---
+++ src/Core.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-License-Identifier: MIT
+
+namespace Nethermind.Libp2p.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/Core/Class1.cs
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-License-Identifier: MIT
+
+namespace Nethermind.Libp2p.Core;
+
+public class Class1
+{
+
+}
