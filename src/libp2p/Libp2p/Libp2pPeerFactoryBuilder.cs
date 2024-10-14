bd22e2bd9ca536537ebfbc85306a550145c2c5b5
Alexey Osipov <me@flcl.me>
2024-10-14 23:26:42 +0300

    Activate gs1.2

--- src/libp2p/Libp2p/Libp2pPeerFactoryBuilder.cs
+++ src/libp2p/Libp2p/Libp2pPeerFactoryBuilder.cs
@@ -36,8 +36,8 @@ public class Libp2pPeerFactoryBuilder : PeerFactoryBuilderBase<Libp2pPeerFactory
             .Over<MultistreamProtocol>()
             .AddAppLayerProtocol<IdentifyProtocol>()
             .AddAppLayerProtocol<PingProtocol>()
-            //.AddAppLayerProtocol<GossipsubProtocolV12>()
-            //.AddAppLayerProtocol<GossipsubProtocolV11>()
+            .AddAppLayerProtocol<GossipsubProtocolV12>()
+            .AddAppLayerProtocol<GossipsubProtocolV11>()
             .AddAppLayerProtocol<GossipsubProtocol>()
             .AddAppLayerProtocol<FloodsubProtocol>();
     }
