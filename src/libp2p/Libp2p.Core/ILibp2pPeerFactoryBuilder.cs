47faca37ebeccadc7f5206f0d20413036b9d34a0
Alexey Osipov <me@flcl.me>
2025-05-17 22:39:26 +0300

    Fix quic

--- src/libp2p/Libp2p.Core/ILibp2pPeerFactoryBuilder.cs
+++ src/libp2p/Libp2p.Core/ILibp2pPeerFactoryBuilder.cs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 Demerzel Solutions Limited
+// SPDX-FileCopyrightText: 2025 Demerzel Solutions Limited
 // SPDX-License-Identifier: MIT
 
 namespace Nethermind.Libp2p.Core;
@@ -8,4 +8,5 @@ public interface ILibp2pPeerFactoryBuilder : IPeerFactoryBuilder
     public ILibp2pPeerFactoryBuilder WithPlaintextEnforced();
     public ILibp2pPeerFactoryBuilder WithPubsub();
     public ILibp2pPeerFactoryBuilder WithRelay();
+    public ILibp2pPeerFactoryBuilder WithQuic();
 }
