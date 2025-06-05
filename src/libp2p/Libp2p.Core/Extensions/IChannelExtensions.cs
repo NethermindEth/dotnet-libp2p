1410531c4c93bc4c6f1fb2abb4f6c193b5024331
Luca Fabbri <luca.fabbri@zucchetti.it>
2025-06-05 11:23:43 +0200

    chore: corrects the white spaces merge issues

--- src/libp2p/Libp2p.Core/Extensions/IChannelExtensions.cs
+++ src/libp2p/Libp2p.Core/Extensions/IChannelExtensions.cs
@@ -9,19 +9,19 @@ namespace Nethermind.Libp2p.Core.Extensions;
 /// </summary>
 public static class IChannelExtensions
 {
-  /// <summary>
-  /// Converts an <see cref="IChannel"/> to a <see cref="Stream"/>.
-  /// </summary>
-  /// <param name="channel">The input channel</param>
-  /// <returns>The Channel as Stream</returns>
-  /// <exception cref="ArgumentNullException">Channel is null throws an <see cref="ArgumentNullException"/></exception>
-  public static Stream AsStream(this IChannel channel)
-  {
-    if (channel is null)
+    /// <summary>
+    /// Converts an <see cref="IChannel"/> to a <see cref="Stream"/>.
+    /// </summary>
+    /// <param name="channel">The input channel</param>
+    /// <returns>The Channel as Stream</returns>
+    /// <exception cref="ArgumentNullException">Channel is null throws an <see cref="ArgumentNullException"/></exception>
+    public static Stream AsStream(this IChannel channel)
     {
-      throw new ArgumentNullException(nameof(channel));
-    }
+        if (channel is null)
+        {
+            throw new ArgumentNullException(nameof(channel));
+        }
 
-    return new ChannelStream(channel);
-  }
+        return new ChannelStream(channel);
+    }
 }
