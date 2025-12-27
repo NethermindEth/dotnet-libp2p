6c027f4a32cfb6e8a38b265aaafcb491ddbf7a5b
Phaesta <Phaesta@Fedora.machine>
2025-12-26 21:46:02 -0600

    Documentation for Nethermind.Libp2p.Core.ISession.DialAsync and Nethermind.Libp2p.Core.LocalPeer.Session.DialAsync Important steps to establish traffic related : https://github.com/NethermindEth/dotnet-libp2p/discussions/161

--- src/libp2p/Libp2p.Core/ISession.cs
+++ src/libp2p/Libp2p.Core/ISession.cs
@@ -10,8 +10,22 @@ public interface ISession
 {
     Multiaddress RemoteAddress { get; }
     Activity? Activity { get; }
-
+    /// <summary>
+    /// Initiate session communication for symmetric protocol without needing a request object, for asymmetric protocol use DialAsync&lt;ISessionProtocol&lt;'req, 'res&gt;, 'req, 'res&gt;('req request) instead
+    /// </summary>
+    /// <typeparam name="TProtocol"></typeparam>
+    /// <param name="token"></param>
+    /// <returns></returns>
     Task DialAsync<TProtocol>(CancellationToken token = default) where TProtocol : ISessionProtocol;
+    /// <summary>
+    /// Initiate session communication
+    /// </summary>
+    /// <typeparam name="TProtocol"></typeparam>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    /// <param name="request"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
     Task<TResponse> DialAsync<TProtocol, TRequest, TResponse>(TRequest request, CancellationToken token = default) where TProtocol : ISessionProtocol<TRequest, TResponse>;
     Task DisconnectAsync();
 }
