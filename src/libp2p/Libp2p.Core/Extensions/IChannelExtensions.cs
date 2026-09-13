2789ba72c0233f8c1299e5795a28920a5b326f27
MrDevRobot <mrdevrobot@entgldb.com>
2026-09-13 19:21:45 +0200

    feat: extends IChannel with AsStream (#134)
    Co-authored-by: Luca Fabbri <luca.fabbri@zucchetti.it>
    Co-authored-by: Alexey Osipov <me@flcl.me>

---
+++ src/libp2p/Libp2p.Core/Extensions/IChannelExtensions.cs
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: 2026 Demerzel Solutions Limited
+// SPDX-License-Identifier: MIT
+
+namespace Nethermind.Libp2p.Core.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="IChannel"/> to convert it into a <see cref="Stream"/>.
+/// </summary>
+public static class IChannelExtensions
+{
+    /// <summary>
+    /// Converts an <see cref="IChannel"/> to a <see cref="Stream"/>.
+    /// </summary>
+    /// <param name="channel">The input channel</param>
+    /// <returns>The channel as a <see cref="Stream"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="channel"/> is null.</exception>
+    public static Stream AsStream(this IChannel channel)
+    {
+        ArgumentNullException.ThrowIfNull(channel);
+
+        return new ChannelStream(channel);
+    }
+}
